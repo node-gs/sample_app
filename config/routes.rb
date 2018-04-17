fda2abae00bb34d87e3ca38af055fcbeb0d8b37d
node-gs <gardesimpson@gmail.com>
2018-04-18 05:36:00 +1000

    finishing static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+  
   get 'static_pages/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-  get 'static_pages/home'
+  get  'static_pages/about'
 
-  get 'static_pages/help'
+  get  'static_pages/contact'
 
-    root 'application#hello'
-  end
 end
